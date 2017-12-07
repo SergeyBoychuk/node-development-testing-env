da50fc6ba87d08818314ca3fd148e64535e35dc3
Sergey Boychuk <sergeboychuk@gmail.com>
2017-12-06 22:55:53 -0500

    Added spies into project folders

---
+++ spies/app.js
@@ -0,0 +1,10 @@
+var db = require('./db.js');
+
+module.exports.handleSignup = (email, password) => {
+  //Check if the email already exists
+  db.saveUser({
+    email,
+    password
+  })
+  //Send that welcome email
+};

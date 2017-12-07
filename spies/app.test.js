06b05641738661c5ad24ee9cf0ccdc49daae5c4f
Sergey Boychuk <sergeboychuk@gmail.com>
2017-12-06 23:55:09 -0500

    finished testing sections, spies

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,9 +1,29 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  }
+  app.__set__('db', db);
+
   it('should call the spy correctly', () => {
     var spy = expect.createSpy();
-    spy('Andrew');
+    spy('Andrew', 25);
     expect(spy).toHaveBeenCalledWith('Andrew', 25);
+  })
+
+  it('should call save user with user object', () => {
+    var email = 'sergeboychuk@gmail.com';
+    var password = 'abc123';
+
+    app.handleSignup(email,password);
+
+    expect(db.saveUser).toHaveBeenCalledWith({
+      email,
+      password
+    })
   })
 })
